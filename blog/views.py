af42ebc882ffe391c804268ace55fe3b503db46f
Jungsik Choi <chjs@skku.edu>
2023-08-20 08:53:49 +0900

    added a page to add new posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from blog.models import Post
 
 
@@ -18,3 +18,15 @@ def post_detail(request, post_id):
         "post": post,
     }
     return render(request, "post_detail.html", context)
+
+
+def post_add(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        post = Post.objects.create(
+            title = title,
+            content = content,
+        )
+        return redirect(f'/posts/{post.id}')
+    return render(request, "post_add.html")
