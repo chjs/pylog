95b4b4b90afc9f57a3d9afbdcdd7fcc4a0a8e38b
Jungsik Choi <chjs@skku.edu>
2023-08-21 23:56:27 +0900

    added commenting

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from blog.models import Post
+from blog.models import Post, Comment
 
 
 # Create your views here.
@@ -13,7 +13,13 @@ def post_list(request):
 
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
-    print(post)
+    if request.method == "POST":
+        comment_content = request.POST["comment"]
+        Comment.objects.create(
+            post = post,
+            content = comment_content,
+        )
+
     context = {
         "post": post,
     }
